f94a0ed9367ebb5bf7240e2345c3f9dbc1661768
Bayu Utomo <by.ciikel@gmail.com>
2020-08-15 02:09:35 +0700

    add upload image

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -29,12 +29,12 @@ class Navigation extends Component {
             >
               Editor
             </div>
-            <div onClick={() => this.navigate("download")}
+            <div onClick={() => this.navigate("preview")}
               className={
-                `${activeNav === 'download' ? "bg-gray-200" : ""}` +
+                `${activeNav === 'preview' ? "bg-gray-200" : ""}` +
                 " px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline cursor-pointer"}
             >
-              Download
+              Preview
             </div>
             <div className="py-2 px-4 cursor-pointer" onClick={() => Store.setSideBarActivedStatus(Store.isSideBarActive ? false : true)}>
               <IosMenu />
--- src/components/templates/ElegancyTemplate.js
+++ src/components/templates/ElegancyTemplate.js
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { css } from 'emotion'
 import { observer } from 'mobx-react'
 import Store from '../../stores/Store'
 
-function ElegancyTemplate() {
-  const { formDatas } = Store
-  const firstColor = css`
-    background-color: ${formDatas.colors[0]};
-  `
-  const secondColor = css`
-    background-color: ${formDatas.colors[1]};
-  `
-  const thirdColor = css`
-    background-color: ${formDatas.colors[2]};
-  `
-  return (
-    <div className="w-full flex relative">
-      <div className="w-full grid grid-rows-2">
-        <div className={secondColor}></div>
-        <div className={thirdColor}></div>
+class ElegancyTemplate extends Component {
+  inputFile = React.createRef()
+  state = {
+    file: null
+  }
+
+  openFormUpload = () => {
+    this.inputFile.current.click()
+  }
+
+  onUploadImage = (data) => {
+    let reader = new FileReader()
+    let file = data.target.files[0]
+    reader.onloadend = () => {
+      this.setState({
+        file: {
+          file, preview: reader.result
+        }
+      })
+    }
+    reader.readAsDataURL(file)
+  }
+
+  render() {
+    const { formDatas } = Store
+    const { file } = this.state
+    const firstColor = css`
+      background-color: ${formDatas.colors[0]};
+    `
+    const secondColor = css`
+      background-color: ${formDatas.colors[1]};
+    `
+    const thirdColor = css`
+      background-color: ${formDatas.colors[2]};
+    `
+
+    return (
+      <div className="w-full flex relative">
+        <div className="w-full grid grid-rows-2">
+          <div className={secondColor}></div>
+          <div className={thirdColor}></div>
+        </div>
+        <div className={firstColor + " absolute h-full w-64 ml-6 flex justify-center"}>
+          <img alt=""
+            src={file ? file.preview : ""}
+            onClick={ this.openFormUpload }
+            className="my-8 w-40 h-40 rounded-full block bg-gray-500 border-0 flex justify-center items-center cursor-pointer"
+          />
+          <input className="hidden" type="file" ref={ this.inputFile } onChange={(data) => this.onUploadImage(data)} />
+        </div>
       </div>
-      <div className={firstColor + " absolute h-full w-64 ml-6"}></div>
-    </div>
-  )
+    )
+  }
 }
 
 export default observer(ElegancyTemplate)
